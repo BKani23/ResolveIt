3ef3ec5672b98336ad9da2ab8cfbebb8af693f85
BKani23 <bkani2304@gmail.com>
2026-03-29 17:51:40 +0200

    feat(api): add Issue Mongoose model and implement GET /api/issues endpoint with error handling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,55 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectToDB } from "./config/db.js";
+import mongoose from "mongoose";
+
 
 // load .env variables
 dotenv.config();
 
 connectToDB();
 
+
+// Initialize Express app
 const app = express();
+
+
+const issueSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: {
+      type: String,
+      enum: ["road", "water", "electricity", "safety", "other"],
+      default: "other",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "resolved"],
+      default: "pending",
+    },
+    location: { type: String },
+    image: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Issue = mongoose.model("Issue", issueSchema);
 
 app.get("/", (req, res) => {
   res.send("ResolveIt API is running...");
+});
+
+
+
+// Get all issues
+app.get("/api/issues", async (req, res) => {
+  try {
+    const issues = await Issue.find();
+    res.json(issues);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
